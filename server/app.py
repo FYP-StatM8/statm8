4025a08438a317c93ea0084566480fc6c5e23f7a
Yash Shah <yashnovember@gmail.com>
2025-11-01 09:50:55 +0530

    fixes

--- server/app.py
+++ server/app.py
@@ -69,7 +69,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # Next.js default
+    allow_origins=["http://localhost:8081", "http://localhost:8082"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
